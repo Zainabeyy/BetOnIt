35ff25bbeb7a27a87a46e80f3cfcb585118b87b6
Zainabeyy <zainabazeem324@gmail.com>
2024-08-21 15:34:14 +0500

    updated the nav

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   const location=window.location.pathname;
   return (
     <nav className="bg-transparent flex justify-between text-white my-4 containerPadding">
-      <a href="/">
+      <Link to="/">
         <img src="../../Logo.png" alt="logo" />
-      </a>
+      </Link>
       <div className="font-medium space-x-8">
-        <a href="/faqs" className={`uppercase ${location==='/faqs' ? 'font-extrabold' : ''}`}>
+        <Link to="/faqs" className={`uppercase ${location==='/faqs' ? 'font-extrabold' : ''}`}>
           faq's
-        </a>
-        <a href="/aboutUs" className={location==='/aboutUs' ? 'font-extrabold' : ''}>About Us</a>
+        </Link>
+        <Link to="/aboutUs" className={location==='/aboutUs' ? 'font-extrabold' : ''}>About Us</Link>
       </div>
     </nav>
   );
